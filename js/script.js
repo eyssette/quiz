37b9223bd4d7ebe15272239ff1902b7063b1af10
eyssette <cedric.eyssette@gmail.com>
2021-02-28 14:12:46 +0100

    updates

--- js/script.js
+++ js/script.js
@@ -1,6 +1,41 @@
 /* Définition des types de question */
 
+function Question(id,regroupement,type,html) {
+	this.id=id;
+	this.regroupement=regroupement;
+	this.type=type;
+	this.html=html;
+	if(type=="vf") {
+		this.calculresultat=1;
+	}
+	if(type=="on") {
+		this.calculresultat=2;
+	}
+}
 
+function htmlVF(id,question) {
+let html=`<div class="card block" id="quiz-q${id}">
+<div class="card-content">
+	<div class="content">
+		${question}
+	</div>
+	<div class="control is-size-5 is-size-6-mobile">
+		<label class="radio">
+			<input type="radio" name="answer">
+			Vrai
+		</label>
+		<label class="radio">
+			<input type="radio" name="answer">
+			Faux
+		</label>
+	</div>
+</div>
+</div>`;
+return html;
+}
+
+
+q1= new Question(1,1,"vf",htmlVF(1,"Test question"));
 
 
 /* Comportement de la boîte modale */
